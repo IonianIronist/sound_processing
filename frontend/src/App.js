d37c76d4d74bf842bc140346e16d0b60385c6877
Nemanja <knnezina@gmail.com>
2022-04-03 18:19:25 +0200

    added the functionality to record and play audio

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
-import {Typography, CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import {
+    Box, Button, Typography, CssBaseline, ThemeProvider, createTheme
+}from "@mui/material";
 import React from 'react'
 
 const theme = createTheme({
@@ -9,19 +11,108 @@ const theme = createTheme({
         primary: {
             main: "#E5E9F0"
         },
+    },
+});
+
+
+class Recorder extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            recording : 0,
+            button_value : {0: 'record', 1 : 'stop recording'},
+            chunks : [],
+        };
+        this.handleRecord = this.handleRecord.bind(this);
+        this.addChunk = this.addChunk.bind(this);
+        this.finishRecording = this.finishRecording.bind(this);
     }
-})
+    
+    componentDidUpdate(_, prevState){
+        if(prevState.recording !== this.state.recording){
+            if(this.state.recording === 1){
+                if(navigator.mediaDevices){
+                    navigator.mediaDevices.getUserMedia({audio: true})
+                    .then(stream => {
+                        this.recorder = new MediaRecorder(stream);
+                        this.recorder.ondataavailable = (e) => this.addChunk(e);
+                        this.recorder.onstop = (e) => this.finishRecording(e);
+                        this.recorder.start();
+                    });
+                
+                }
+                else{
+                    alert("Audio Recording Source Required.");
+                }
+            }
+            else if(this.state.recording === 0){
+                this.recorder.stop();
+            }
+        }
+    }
+
+    addChunk(e){
+        let new_chunks = [...this.state.chunks];
+        new_chunks.push(e.data);
+        this.setState({chunks: new_chunks});
+    }
+    
+    finishRecording(e){
+        let blob = new Blob(this.state.chunks, {'type': 'audio/ogg; codecs=opus'});
+        this.setState({chunks : []});
+        this.props.saveClip(blob);
+    }
+
+    handleRecord(){
+        this.setState(oldState => ({recording: (oldState.recording) ? 0 : 1}))
+    }
+
+
+    render(){
+        return(
+            <Button 
+                variant="outlined"
+                onClick={this.handleRecord}
+            >
+                {this.state.button_value[this.state.recording]}
+            </Button>
+        )
+    }
+}
 
 
 class App extends React.Component {
     constructor(props){
         super(props)
+        this.state = {
+            src: ''
+        }
+        this.onSaveClip = this.onSaveClip.bind(this);
     }
+
+    onSaveClip(blob){
+        this.setState({
+            clip: blob,
+            src: URL.createObjectURL(blob),
+        });
+    }
+
     render(){
         return (
             <ThemeProvider theme={theme}>
                 <CssBaseline/>
-                <Typography variant='h1' color='primary'>This is the main page</Typography>
+                <Box
+                     display="flex"
+                     justifyContent="center"
+                     alignItems="center"
+                     flexDirection="column"
+                >
+                <Typography variant='h3' color='primary'>This is the main page</Typography>
+                <Box>
+                    <Recorder saveClip={this.onSaveClip} />
+                </Box>
+                    <audio controls src={this.state.src}/>
+                </Box>
             </ThemeProvider>
         );
     }
